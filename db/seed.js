26b49282283bf942e1ddae394bf8f5b78057cec4
surelis segarra <sdarianne34@gmail.com>
2020-03-27 10:00:15 -0700

    created seed data for reviewer

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,9 @@
 const Studio = require('../lib/models/Studio');
 const chance = require('chance').Chance();
 const Actor = require('../lib/models/Actor');
+const Reviewer = require('../lib/models/Reviewer');
 
-module.exports = async({ studiosToCreate = 10, actorsToCreate = 10 } = {}) => {
+module.exports = async({ studiosToCreate = 10, actorsToCreate = 10, reviewersToCreate = 10 } = {}) => {
   const studios = await Studio.create([...Array(studiosToCreate)].map(() => ({
     name: chance.name(),
     address: {
@@ -16,6 +17,11 @@ module.exports = async({ studiosToCreate = 10, actorsToCreate = 10 } = {}) => {
     name: chance.name(),
     dob: chance.date(),
     pob: chance.city()
+  })));
+
+  const reviewer = await Reviewer.create([...Array(reviewersToCreate)].map(() => ({
+    name: chance.name(),
+    company: chance.company()
   })));
 };
 
